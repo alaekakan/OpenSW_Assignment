585673196563e587939472577fff198c3c3f22f6
JuBohyun <wnqhgus1209@gmail.com>
2025-11-10 19:17:30 +0900

    add my basic ops

---
+++ week09_git/include/myops.h
@@ -0,0 +1,11 @@
+#ifndef MYOPS_H
+#define MYOPS_H
+
+#include <stdio.h>
+double myadd(double a, double b);
+double mysub(double a, double b);
+double mymul(double a, double b);
+double mydiv(double a, double b);
+
+#endif  // MYOPS_H
+// h파일은 설계도!! (필요한 부품 선언)
