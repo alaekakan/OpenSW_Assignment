770a7b2093fbbadd1ca22661b9427b5fe6b0ddcc
JuBohyun <wnqhgus1209@gmail.com>
2025-11-10 19:44:54 +0900

    add main(), print good bye

--- week09_git/src/myapp.c
+++ week09_git/src/myapp.c
@@ -25,9 +25,10 @@ double calculate(char* op, double a, double b) {
 int main(void) {
     double a, b;
     char op[3];
-    printf("~~~ Welcome to my calculator ~~~");
+    printf("~~~ Welcome to my calculator ~~~\n\n");
     printf("Enter expression (e.g 1 + 2): ");
     scanf("%lf %s %lf", &a, op, &b);
     printf("Result = %.2lf\n", calculate(op, a, b));
+    printf("~~~ Good bye. See you again ~~~\n\n");
     return 0;
 }
